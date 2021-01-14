6b8aeb477dcc5e06979565a7d8a01e5c6afabbf6
Areias Alexandre <alexandre.dsa02@gmail.com>
2021-01-14 12:59:00 +0100

    modif ambiance FK16

--- migrations/Version20200904120630.php
+++ migrations/Version20200904120630.php
@@ -20,7 +20,7 @@ final class Version20200904120630 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE devis (id INT AUTO_INCREMENT NOT NULL, etage VARCHAR(255) DEFAULT NULL, lieu_livraison VARCHAR(255) NOT NULL, duree_loc INT NOT NULL, delai_prev INT NOT NULL, etat_devis VARCHAR(255) NOT NULL, date_livraison DATE NOT NULL, type_devis VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE IF NOT EXISTS devis (id INT AUTO_INCREMENT NOT NULL, etage VARCHAR(255) DEFAULT NULL, lieu_livraison VARCHAR(255) NOT NULL, duree_loc INT NOT NULL, delai_prev INT NOT NULL, etat_devis VARCHAR(255) NOT NULL, date_livraison DATE NOT NULL, type_devis VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
